98400ccc3fd7abd2378b4231291ac171ca8c113e
Radhika Mittal <106824237+Radhi2422@users.noreply.github.com>
2026-08-15 14:20:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,18 +9,18 @@ const logger = require("./src/logs/logger.js");
 connectDB();
 
 const PORT =process.env.PORT || 5001;
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.on("clientError", (err, socket) => {
-    console.error("Client Error:", err.message);
-    socket.destroy();
-});
-server.listen(5003);
-// app.listen(PORT, () => {
-//     // logger.info(
-//     //     `Server running on ${PORT}`
-//     // );
-//     console.log(
-//         `Server running on ${PORT}`
-//     );
+// server.on("clientError", (err, socket) => {
+//     console.error("Client Error:", err.message);
+//     socket.destroy();
 // });
+// server.listen(5003);
+app.listen(PORT, () => {
+    // logger.info(
+    //     `Server running on ${PORT}`
+    // );
+    console.log(
+        `Server running on ${PORT}`
+    );
+});
