ca80b66f911fa3c42c7fe69691e3c595ab507ab0
Radhika Mittal <106824237+Radhi2422@users.noreply.github.com>
2026-08-15 14:32:53 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 
 import './index.css'
 import App from './App.jsx'
-import { store } from './redux/store'
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
